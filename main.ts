5313f02c85e8e7ca99be6cab711f79f166d16e17
Vítek Dolanský <vitek.dolansky.024@pslib.cz>
2025-05-17 15:45:26 +0200

    pridany radio receive a fungovani motoru podle naklonu druheho microbita

--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
+radio.setGroup(23)
 
+let x = 0
+let y = 0
+let trim = 0
+
+function setMotors(leftSpeed: number, rightSpeed: number) {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, leftSpeed)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, rightSpeed)
+}
+
+radio.onReceivedValue(function (name, value) {
+    if (name === "x") {
+        x = value
+    } else if (name === "y") {
+        y = value
+    }
+})
+
+basic.forever(function () {
+    let leftSpeed = x - y
+    let rightSpeed = x + y
+
+    if (leftSpeed > 255) leftSpeed = 255
+    if (leftSpeed < -255) leftSpeed = -255
+    if (rightSpeed > 255) rightSpeed = 255
+    if (rightSpeed < -255) rightSpeed = -255
+
+    setMotors(leftSpeed, rightSpeed)
+
+    //basic.showNumber(y)
+
+    basic.pause(30)
+})
